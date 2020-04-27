a0e343f36b96239bbf1ab8c1fda4c1d6a2adc93f
Max <hktxt2011@gmail.com>
2020-04-27 14:32:11 +0800

    add color.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='VisionToolKit',
+      version='0.1',
+      description='toolkit for computer vision.',
+      url='http://github.com/tongling/VisionToolKit',
+      author='hktxt',
+      author_email='hktxt2011@gmail.com',
+      license='MIT',
+      packages=['VisionToolKit'],
+      zip_safe=False)
