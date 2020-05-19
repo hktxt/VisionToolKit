3d08a2d265b3b100ea843407005ada7b1699364c
Max <hktxt2011@gmail.com>
2020-05-19 11:34:58 +0800

    Update draw.py

--- visiontoolkit/draw.py
+++ visiontoolkit/draw.py
@@ -3,7 +3,7 @@ import numpy as np
 from copy import deepcopy
 
 
-def draw_bbox(image, bboxes, class_ids=None, class_idx_to_name=None,
+def draw_bbox(image, bboxes, class_ids=None, class_idx_to_name=None, scores=None,
               box_color=(255, 0, 0), text_color=(255, 255, 255), thickness=2):
     """
     draw bounding box in the image.
@@ -13,7 +13,9 @@ def draw_bbox(image, bboxes, class_ids=None, class_idx_to_name=None,
     bboxes: bounding boxes, list: [[x1, y1, x2, y2], ..., [x1, y1, x2, y2]].
            e.g. [[97, 12, 247, 212], [[90, 17, 242, 219]]].
     class_ids: class id of every bbox, list of int, e.g. [18, 17].
-    class_idx_to_name: name of class_id, dict: {17: 'cat', 18: 'dog'}
+    class_idx_to_name: name of class_id, dict: {17: 'cat', 18: 'dog'},
+    scores: score of bbox, list. [0.99, 0.98, 0.57, ..., 0.78], should have same
+    length of bboxes.
     box_color: bbox color, tuple: (255, 0, 0), or length of bboxs tuple
     text_color: text color, tuple: (255, 255, 255)
     thickness: line thickness, int, e.g. 2
@@ -37,22 +39,38 @@ def draw_bbox(image, bboxes, class_ids=None, class_idx_to_name=None,
     img = np.ascontiguousarray(deepcopy(image))
     if class_ids:
         assert len(bboxes) == len(class_ids), 'length error, len(bboxes) should == len(class_id).'
+    if scores:
+        assert len(bboxes) == len(scores), 'length error, len(bboxes) should == len(scores).'
 
-        for bbox, class_id in zip(bboxes, class_ids):
-            x_min, y_min, x_max, y_max = list(map(int, bbox))
-            class_name = class_idx_to_name[class_id] if class_idx_to_name else str(class_id)
-            #colr = box_color
-            cv2.rectangle(img, (x_min, y_min), (x_max, y_max), box_color, thickness)
-            ((text_width, text_height), _) = cv2.getTextSize(class_name, cv2.FONT_HERSHEY_SIMPLEX, 0.35, 1)
-            cv2.rectangle(img, (x_min, y_min - int(1.3 * text_height)), (x_min + text_width, y_min), box_color, -1)
-            cv2.putText(img, class_name, (x_min, y_min - int(0.3 * text_height)), cv2.FONT_HERSHEY_SIMPLEX, 0.35,
+    n = len(bboxes)
+    for i in range(n):
+        bbox = bboxes[i]
+        x_min, y_min, x_max, y_max = list(map(int, bbox))
+        cv2.rectangle(img, (x_min, y_min), (x_max, y_max), box_color, thickness)
+        string = None
+        if class_ids:
+            class_name = class_idx_to_name[class_ids[i]] if class_idx_to_name else str(class_ids[i])
+            string = class_name
+        if scores:
+            score = scores[i]
+            if string:
+                string = string + ': ' + str(score)
+            else:
+                string = str(score)
+
+        if string:
+            ((text_width, text_height), _) = cv2.getTextSize(string, cv2.FONT_HERSHEY_SIMPLEX, 0.35, 1)
+            # up
+            # cv2.rectangle(img, (x_min, y_min - int(1.3 * text_height)), (x_min + text_width, y_min), box_color, -1)
+            # cv2.putText(img, string, (x_min, y_min - int(0.3 * text_height)), cv2.FONT_HERSHEY_SIMPLEX, 0.35,
+            # text_color,
+            # lineType=cv2.LINE_AA)
+            # down
+            cv2.rectangle(img, (x_min + 1, y_min), (x_min + 1 + text_width, y_min + int(1.3 * text_height)), box_color,
+                          -1)
+            cv2.putText(img, string, (x_min + 1, y_min + int(1.2 * text_height)), cv2.FONT_HERSHEY_SIMPLEX, 0.35,
                         text_color,
                         lineType=cv2.LINE_AA)
-    else:
-        for bbox in bboxes:
-            x_min, y_min, x_max, y_max = list(map(int, bbox))
-            #colr = box_color
-            cv2.rectangle(img, (x_min, y_min), (x_max, y_max), box_color, thickness)
 
     return img
 
